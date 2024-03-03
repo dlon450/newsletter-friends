247fa5017f80a718bfc4f819aefbae8cb73d5597
dlon450 <dlon450@aucklanduni.ac.nz>
2024-03-02 19:33:17 -0500

    added reminder email

--- reminder.py
+++ reminder.py
@@ -84,7 +84,7 @@ if __name__ == "__main__":
 
     load_dotenv()
     sender = os.getenv("GMAIL_ADDRESS")
-    recipients = ast.literal_eval(os.getenv("RECIPIENT"))
+    recipients = ast.literal_eval(os.getenv("RECIPIENT_SPARK"))
     recipients_spark = ast.literal_eval(os.getenv("RECIPIENT_SPARK"))
     password = os.getenv("APP_PASSWORD")
     sheet_id = os.getenv("SHEET_ID")
